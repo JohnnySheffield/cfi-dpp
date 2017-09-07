8fdda507c6649f25bdd456375e1af220a0afd978
Nikola Jokic <nikola@kip.investments>
2017-09-07 12:36:37 +0200

    fixes non used IMintable.sol

--- contracts/Interfaces/IToken.sol
+++ contracts/Interfaces/IToken.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.13;
 
-contract IToken {
+import "./IMintableToken.sol";
+
+contract IToken is IMintableToken {
   function totalSupply() constant returns (uint256 totalSupply);
-  function mintTokens(address _to, uint256 _amount) {}
 }
